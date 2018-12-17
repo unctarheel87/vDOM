e0c2a3b76f12ba942d517a29e9a959631db5240e
John Tillman <john.w.tillman@gmail.com>
2018-12-16 21:20:38 -0500

    event listeners

--- vDOM.js
+++ vDOM.js
@@ -2,7 +2,7 @@
 const jsx_1 = (
   { type: 'div', props: { className: 'container' }, children: [
     { type: 'h1', props: {}, children: ['Hello'] },
-    { type: 'h2', props: {}, children: ['World!'] },
+    { type: 'h2', props: {onClick: () => console.log('hey I am and h2!')}, children: ['World!'] },
     { type: 'ul', props: {}, children: [
       { type: 'li', props: {}, children: ['item 1'] },
       { type: 'li', props: {}, children: ['item 2'] }
@@ -22,22 +22,19 @@ const jsx_2 = (
 
 // helper functions
 
-function changed(node1, node2) {
-  return typeof node1 !== typeof node2 ||
-         typeof node1 === 'string' && node1 !== node2 ||
-         node1.type !== node2.type
-}
+const changed = (node1, node2) => ( 
+  typeof node1 !== typeof node2 ||
+  typeof node1 === 'string' && node1 !== node2 ||
+  node1.type !== node2.type
+);
 
-function setProps($el, props) {
-  const set_Props = (value, key) => {
-    if(key === 'className') {
-      $el.setAttribute('class', value);
-    } else {
-      $el.setAttribute(key, value);
-    }
-  }
-  R.forEachObjIndexed(set_Props, props);
-}
+const isEventProp = name => (
+  /^on/.test(name)
+);
+
+const getEventName = name => (
+  R.compose(R.toLower, R.slice)(2, Infinity, name)
+); 
 
 const toElement = node => createElement(node); 
 const append_Child = (el, node) => el.appendChild(node);
@@ -50,6 +47,7 @@ function createElement(node) {
   }
   const $el = document.createElement(node.type);
   setProps($el, node.props);
+  addEventListeners($el, node.props);
   R.pipe(R.map(toElement), R.forEach(R.partial(append_Child, [$el])))(node.children);
   return $el;
 }
@@ -77,6 +75,31 @@ function updateElement($parent, newNode, oldNode, index = 0) {
     (newLength > oldLength ? newNode.children : oldNode.children)
       .forEach(R.partial(update_Element, [index]));
   }
+}
+
+// handle props
+
+function setProps($el, props) {
+  const set_Props = (value, key) => {
+    if(key === 'className') {
+      $el.setAttribute('class', value);
+    } else {
+      $el.setAttribute(key, value);
+    }
+  }
+  R.forEachObjIndexed(set_Props, props);
+}
+
+// handle events
+
+function addEventListeners($target, props) {
+  const add_EventListeners = (value, key) => {
+    if(isEventProp(key)) {
+      console.log(getEventName(key), value)
+      $target.addEventListener(getEventName(key), value)
+    }
+  }
+  R.forEachObjIndexed(add_EventListeners, props);
 }
 
 const $root = document.getElementById('root');
